91019966762969818fe17ac9c7b0c36967a0c311
Asicreativedude <Asicreativedude@gmail.com>
2023-12-24 16:17:35 +0700

    mobile paly video

--- src/home.ts
+++ src/home.ts
@@ -2,8 +2,9 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
-//init video players
-const controls = `
+document.addEventListener('DOMContentLoaded', () => {
+	//init video players
+	const controls = `
 <div class="plyr__controls">
 <button type="button" class="plyr__control cd-video-btn" aria-label="Play video" data-plyr="play">
 	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon--pressed" role="presentation">
@@ -21,185 +22,252 @@ const controls = `
     </button>
 
 	`;
-//@ts-ignore
-const players = Array.from(document.querySelectorAll('.js-player')).map(
-	(p) =>
-		//@ts-ignore
-		new Plyr(p, {
-			controls,
-		})
-);
+	//@ts-ignore
+	const players = Array.from(document.querySelectorAll('.js-player')).map(
+		(p) =>
+			//@ts-ignore
+			new Plyr(p, {
+				controls,
+			})
+	);
 
-const allVideos = document.querySelectorAll('.plyr--video');
-
-allVideos.forEach((player) => {
-	player.addEventListener('click', () => {
-		(player.querySelector('.cd-video-btn-text')! as HTMLSpanElement)
-			.textContent === 'Play Video'
-			? ((
-					player.querySelector('.cd-video-btn-text')! as HTMLSpanElement
-			  ).textContent = 'Pause Video')
-			: ((
-					player.querySelector('.cd-video-btn-text')! as HTMLSpanElement
-			  ).textContent = 'Play Video');
+	const mobilePlayBtn = document.querySelector('.cd-video-btn.mobile-only')!;
+	mobilePlayBtn.addEventListener('click', () => {
+		testimonialSwiper.slides[testimonialSwiper.activeIndex].querySelector(
+			'video'
+		)!.paused
+			? (testimonialSwiper.slides[testimonialSwiper.activeIndex]
+					.querySelector('video')!
+					.play(),
+			  (mobilePlayBtn.querySelector('.mobile-video-btn-text')!.textContent =
+					'Pause Video'),
+			  mobilePlayBtn
+					.querySelector('.mobile-video-play-icon')!
+					.classList.remove('active'),
+			  mobilePlayBtn
+					.querySelector('.mobile-video-pause-icon')!
+					.classList.add('active'))
+			: (testimonialSwiper.slides[testimonialSwiper.activeIndex]
+					.querySelector('video')!
+					.pause(),
+			  (mobilePlayBtn.querySelector('.mobile-video-btn-text')!.textContent =
+					'Play Video'),
+			  mobilePlayBtn
+					.querySelector('.mobile-video-play-icon')!
+					.classList.add('active'),
+			  mobilePlayBtn
+					.querySelector('.mobile-video-pause-icon')!
+					.classList.remove('active'));
 	});
-});
-//carousel
-const swiperNavBtns = document.querySelectorAll(
-	'.swiper-nav-btn'
-) as NodeListOf<HTMLButtonElement>;
 
-//@ts-ignore
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	effect: 'fade',
-	speed: 500,
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
+	const allVideos = document.querySelectorAll('.plyr--video');
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-btn-next',
-		prevEl: '.swiper-btn-prev',
-		disabledClass: 'disabled',
-	},
-});
+	allVideos.forEach((player) => {
+		player.addEventListener('click', () => {
+			(player.querySelector('.cd-video-btn-text')! as HTMLSpanElement)
+				.textContent === 'Play Video'
+				? (((
+						player.querySelector('.cd-video-btn-text')! as HTMLSpanElement
+				  ).textContent = 'Pause Video'),
+				  (document.querySelector('.mobile-video-btn-text')!.textContent =
+						'Pause Video'),
+				  mobilePlayBtn
+						.querySelector('.mobile-video-play-icon')!
+						.classList.remove('active'),
+				  mobilePlayBtn
+						.querySelector('.mobile-video-pause-icon')!
+						.classList.add('active'))
+				: (((
+						player.querySelector('.cd-video-btn-text')! as HTMLSpanElement
+				  ).textContent = 'Play Video'),
+				  (document.querySelector('.mobile-video-btn-text')!.textContent =
+						'Play Video'),
+				  mobilePlayBtn
+						.querySelector('.mobile-video-play-icon')!
+						.classList.add('active'),
+				  mobilePlayBtn
+						.querySelector('.mobile-video-pause-icon')!
+						.classList.remove('active'));
+		});
+	});
+	//carousel
+	const swiperNavBtns = document.querySelectorAll(
+		'.swiper-nav-btn'
+	) as NodeListOf<HTMLButtonElement>;
 
-swiper.on('slideChange', () => {
-	swiper.slides[swiper.activeIndex].querySelector('video')!.pause();
+	//@ts-ignore
+	const testimonialSwiper = new Swiper('.swiper', {
+		// Optional parameters
+		effect: 'fade',
+		speed: 500,
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-btn-next',
+			prevEl: '.swiper-btn-prev',
+			disabledClass: 'disabled',
+		},
+	});
+
+	testimonialSwiper.on('slideChange', () => {
+		testimonialSwiper.slides[testimonialSwiper.activeIndex]
+			.querySelector('video')!
+			.pause();
+		swiperNavBtns.forEach((btn) => {
+			if (btn.classList.contains('disabled')) {
+				btn.querySelector('.swiper-arrow-c')!.classList.add('disabled');
+				btn.querySelector('.swiper-navigation-text')!.classList.add('hide');
+			} else {
+				btn.querySelector('.swiper-navigation-text')!.classList.remove('hide');
+				btn.querySelector('.swiper-arrow-c')!.classList.remove('disabled');
+			}
+		});
+		const nextSlide =
+			testimonialSwiper.slides[testimonialSwiper.activeIndex + 1];
+		const prevSlide =
+			testimonialSwiper.slides[testimonialSwiper.activeIndex - 1];
+		if (nextSlide) {
+			const nextSlideText = nextSlide
+				.querySelector('.testimonial-slide')!
+				.getAttribute('testimonial-company');
+			swiperNavBtns[1].querySelector('.testimonial-company-name')!.textContent =
+				nextSlideText;
+		}
+		if (prevSlide) {
+			const prevSlideText = prevSlide
+				.querySelector('.testimonial-slide')!
+				.getAttribute('testimonial-company');
+			swiperNavBtns[0].querySelector('.testimonial-company-name')!.textContent =
+				prevSlideText;
+		}
+	});
+
 	swiperNavBtns.forEach((btn) => {
-		if (btn.classList.contains('disabled')) {
-			btn.querySelector('.swiper-arrow-c')!.classList.add('disabled');
-			btn.querySelector('.swiper-navigation-text')!.classList.add('hide');
-		} else {
-			btn.querySelector('.swiper-navigation-text')!.classList.remove('hide');
-			btn.querySelector('.swiper-arrow-c')!.classList.remove('disabled');
-		}
-	});
-	const nextSlide = swiper.slides[swiper.activeIndex + 1];
-	const prevSlide = swiper.slides[swiper.activeIndex - 1];
-	if (nextSlide) {
-		const nextSlideText = nextSlide
-			.querySelector('.testimonial-slide')!
-			.getAttribute('testimonial-company');
-		swiperNavBtns[1].querySelector('.testimonial-company-name')!.textContent =
-			nextSlideText;
-	}
-	if (prevSlide) {
-		const prevSlideText = prevSlide
-			.querySelector('.testimonial-slide')!
-			.getAttribute('testimonial-company');
-		swiperNavBtns[0].querySelector('.testimonial-company-name')!.textContent =
-			prevSlideText;
-	}
-});
-
-swiperNavBtns.forEach((btn) => {
-	btn.addEventListener('mouseover', () => {
-		if (btn.classList.contains('disabled')) {
-			return;
-		}
-		btn.querySelector('.swiper-arrow-c')!.classList.add('hover');
-	});
-	btn.addEventListener('mouseout', () => {
-		btn.querySelector('.swiper-arrow-c')!.classList.remove('hover');
-	});
-});
-
-function setGsapScroll() {
-	gsap.set('.orange-circle', {
-		background: '#d9d9d9',
-	});
-	gsap.set('.grey-line', {
-		height: '100%',
+		btn.addEventListener('mouseover', () => {
+			if (btn.classList.contains('disabled')) {
+				return;
+			}
+			btn.querySelector('.swiper-arrow-c')!.classList.add('hover');
+		});
+		btn.addEventListener('mouseout', () => {
+			btn.querySelector('.swiper-arrow-c')!.classList.remove('hover');
+		});
 	});
 
-	ScrollTrigger.create({
-		trigger: '.home-solution-grid',
-		start: 'top 75%',
-		onEnter: () => {
-			gsap.to('.orange-circle', {
-				background: '#ff872e',
-				duration: 0.3,
-				ease: 'power2.out',
-			});
-		},
-		onLeaveBack: () => {
-			gsap.to('.orange-circle', {
-				background: '#d9d9d9',
-				duration: 0.3,
-				ease: 'power2.out',
-			});
-		},
-	});
+	function setGsapScroll() {
+		gsap.set('.orange-circle', {
+			background: '#d9d9d9',
+		});
+		gsap.set('.grey-line', {
+			height: '100%',
+		});
 
-	gsap.to('.orange-line', {
-		height: '100%',
-		scrollTrigger: {
+		ScrollTrigger.create({
 			trigger: '.home-solution-grid',
-			start: 'top 65%',
-			end: 'bottom',
-			scrub: 1,
-		},
+			start: 'top 75%',
+			onEnter: () => {
+				gsap.to('.orange-circle', {
+					background: '#ff872e',
+					duration: 0.3,
+					ease: 'power2.out',
+				});
+			},
+			onLeaveBack: () => {
+				gsap.to('.orange-circle', {
+					background: '#d9d9d9',
+					duration: 0.3,
+					ease: 'power2.out',
+				});
+			},
+		});
+
+		gsap.to('.orange-line', {
+			height: '100%',
+			scrollTrigger: {
+				trigger: '.home-solution-grid',
+				start: 'top 65%',
+				end: 'bottom',
+				scrub: 1,
+			},
+		});
+
+		gsap.to('.grey-line', {
+			height: '0%',
+			scrollTrigger: {
+				trigger: '.home-solution-grid',
+				start: 'top 65%',
+				end: 'bottom',
+				scrub: 1,
+			},
+		});
+	}
+
+	const solutionVideos = document.querySelectorAll('.home-solution-video');
+
+	solutionVideos.forEach((video) => {
+		ScrollTrigger.create({
+			trigger: video,
+			start: 'top 60%',
+			end: 'bottom 50%',
+			onEnter: () => {
+				(video.querySelector('video')! as HTMLVideoElement).play();
+			},
+			onLeave: () => {
+				(video.querySelector('video')! as HTMLVideoElement).pause();
+			},
+			onLeaveBack: () => {
+				(video.querySelector('video')! as HTMLVideoElement).pause();
+			},
+			onEnterBack: () => {
+				(video.querySelector('video')! as HTMLVideoElement).play();
+			},
+		});
 	});
 
-	gsap.to('.grey-line', {
-		height: '0%',
-		scrollTrigger: {
-			trigger: '.home-solution-grid',
-			start: 'top 65%',
-			end: 'bottom',
-			scrub: 1,
-		},
-	});
-}
-function setMobileSwiperNavigation() {
-	const distance = (
-		document.querySelector('.testimonial-video-c') as HTMLDivElement
-	).offsetHeight;
-	console.log(distance);
-	(
-		document.querySelector('.swiper-navigation-c') as HTMLDivElement
-	).style.top = `${distance / 10 + 1.5}rem`;
-}
-let isMobile = window.innerWidth < 992;
+	//mobile
 
-if (!isMobile) {
-	setGsapScroll();
-} else {
-	setMobileSwiperNavigation();
-}
+	function setMobileTestimonialSwiperNavigation() {
+		const distance = (
+			document.querySelector('.testimonial-video-c') as HTMLDivElement
+		).offsetHeight;
+		(
+			document.querySelector('.swiper-navigation-c') as HTMLDivElement
+		).style.top = `${distance / 10 + 1.5}rem`;
+	}
 
-window.addEventListener('resize', () => {
-	isMobile = window.innerWidth < 768;
+	function setExcellenceSwiper() {
+		//@ts-ignore
+		const testimonialSwiper = new Swiper('.excellence-grid-wrapper', {
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+			wrapperClass: 'excellence-grid',
+			slideClass: 'excellence-item-c',
+			scrollbar: {
+				el: '.excellenceswiper-scrollbar',
+			},
+		});
+	}
+	let isMobile = window.innerWidth < 992;
+
 	if (!isMobile) {
 		setGsapScroll();
 	} else {
-		setMobileSwiperNavigation();
+		setMobileTestimonialSwiperNavigation();
+		setExcellenceSwiper();
 	}
-});
 
-const solutionVideos = document.querySelectorAll('.home-solution-video');
-
-solutionVideos.forEach((video) => {
-	ScrollTrigger.create({
-		trigger: video,
-		start: 'top 60%',
-		end: 'bottom 50%',
-		onEnter: () => {
-			(video.querySelector('video')! as HTMLVideoElement).play();
-		},
-		onLeave: () => {
-			(video.querySelector('video')! as HTMLVideoElement).pause();
-		},
-		onLeaveBack: () => {
-			(video.querySelector('video')! as HTMLVideoElement).pause();
-		},
-		onEnterBack: () => {
-			(video.querySelector('video')! as HTMLVideoElement).play();
-		},
+	window.addEventListener('resize', () => {
+		isMobile = window.innerWidth < 992;
+		if (!isMobile) {
+			setGsapScroll();
+		} else {
+			setMobileTestimonialSwiperNavigation();
+			setExcellenceSwiper();
+		}
 	});
 });
